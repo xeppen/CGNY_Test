a5083c10a390376cfe1d54f12483e2f75d3d4e01
Sebastian Ljungberg <sebastian.ljungberg@widespace.com>
2016-06-30 19:32:53 +0200

    Add new tableviewcontroller to display the search terms

---
+++ CGNY/CGNY/CGNYSearchTermTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CGNYSearchTermTableViewController.h
+//  CGNY
+//
+//  Created by Sebastian Ljungberg on 30/06/16.
+//  Copyright © 2016 Xeppen Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGNYSearchTermTableViewController : UITableViewController
+
+@end
---
+++ CGNY/CGNY/CGNYSearchTermTableViewController.m
@@ -0,0 +1,26 @@
+//
+//  CGNYSearchTermTableViewController.m
+//  CGNY
+//
+//  Created by Sebastian Ljungberg on 30/06/16.
+//  Copyright © 2016 Xeppen Productions. All rights reserved.
+//
+
+#import "CGNYSearchTermTableViewController.h"
+
+@interface CGNYSearchTermTableViewController ()
+
+@property (nonatomic, strong) NSArray *searchTerms;
+@end
+
+@implementation CGNYSearchTermTableViewController
+
+#pragma mark - Initialization
+
+#pragma mark - UITableViewDatasource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+@end
